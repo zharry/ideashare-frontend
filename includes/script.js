e6f121978dc6cdfe7a8a8780388bee8e4468dd21
Harry <zh.harry@yahoo.ca>
2016-09-17 13:18:32 +0000

    Frontend Testing

--- includes/script.js
+++ includes/script.js
@@ -1,7 +1,7 @@
 function createIdea() {
     var author = document.getElementById("createideaauthor").value;
     var title = document.getElementById("createideatitle").value;
-    var content = document.getElementById("createideaauthor").value;
+    var content = document.getElementById("createideacontent").value;
     var tags = document.getElementById("createideatags").value;
     api("ideas/create", [title, author, content, tags], alert);
 }
