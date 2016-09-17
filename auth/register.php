2fbc4debc8d30a6c5d2519ebdaa8133cf9f89e5c
Harry <zh.harry@yahoo.ca>
2016-09-17 02:53:16 +0000

    Testing Registration

--- auth/register.php
+++ auth/register.php
@@ -5,14 +5,14 @@
     if (!empty($_POST)) {
         
         $prep = mysqli_prepare($conn, "INSERT INTO is_users (username, email, frist, last, password) 
-            VALUES (?, ?, ?, ?, ?, ?);
+            VALUES (?, ?, ?, ?, ?);
         ");
         mysqli_stmt_bind_param($prep, 'sssss', $_POST["username"], $_POST["email"], $_POST["frist"], $_POST["last"], $_POST["password"]);
         
         if (mysqli_stmt_execute($prep) && mysqli_stmt_close($prep)) {
             die("Registered!");
         } else {
-            die("Registration Failed! Error: " . mysqli_stmt_error($prep));
+            die("Registration Failed!");
         }
         
         
