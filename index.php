d0074260a6cf43a1e4aad86bb3eff11dc064a1d8
Harry <zh.harry@yahoo.ca>
2016-09-17 11:10:54 +0000

    Frontend backup

--- index.php
+++ index.php
@@ -7,10 +7,30 @@
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <title>IdeaShare</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+        <link rel="stylesheet" href="/includes/style.css">
     </head>
     <body>
-        <h1>Hello, world!</h1>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <form class="navbar-form navbar-right">
+                        <div class="form-group">
+                            <textarea type="text" id="topsearch" class="form-control" placeholder="Search or Create Idea" onkeyup="expandTopbar()" style="height: 34px"></textarea>
+                        </div>
+                        <button type="button" id="topbuttona" class="btn btn-default" style="height: 34px">Search Ideas</button>
+                        <button type="button" id="topbuttonb" class="btn btn-default" style="height: 34px">Create Idea</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
     
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -18,5 +38,6 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <!--Self-includes-->
         <script src="/includes/api.js"></script>
+        <script src="/includes/script.js"></script>
     </body>
 </html>
