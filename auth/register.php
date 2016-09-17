cbf112442d6b4b8c3ef8099c2d17c336cbe6f6f2
Harry <zh.harry@yahoo.ca>
2016-09-17 02:37:03 +0000

    Testing Registration

---
+++ auth/register.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once '../includes/connection.php';
+
+    if (isset($_POST)) {
+        $prep = mysqli_prepare($conn, "INSERT INTO is_users (username, email, frist, last, password) 
+            VALUES (?, ?, ?, ?, ?, ?);
+        ");
+        mysqli_stmt_bind_param($prep, 'sssss', $_POST["username"], $_POST["email"], $_POST["frist"], $_POST["last"], $_POST["password"]);
+        mysqli_stmt_execute($stmt);
+        
+        echo "Registered!";
+        die();
+    }
+
+?>
+
+<form action="register.php" method="post">
+Username: <input type="text" name="username"><br>
+Email: <input type="text" name="email"><br>
+First: <input type="text" name="frist"><br>
+Last: <input type="text" name="last"><br>
+Pass: <input type="password" name="password"><br>
+<input type="submit" value="Submit">
+</form>
--- includes/connection.php
+++ includes/connection.php
@@ -16,8 +16,6 @@
     // Check connection
     if (mysqli_connect_errno()) {
         die(mysqli_connect_error());
-    } else {
-        echo "MySQL Working";
     }
 
 ?>
