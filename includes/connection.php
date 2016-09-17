d7320f142ff746f1f9be091918d7835e5b5b6e7c
Harry <zh.harry@yahoo.ca>
2016-09-17 02:16:12 +0000

    Testing MySQL

---
+++ includes/connection.php
@@ -0,0 +1,23 @@
+<?php 
+
+    /** How to access MySQL
+     *   1. Use the following require_once:
+     *      require_once '/etc/mysql-creds/ideashare-mysql.inc.php';
+     *   2. Use the array named $ISM
+     *   3. Fields: host,     user,     pass,     db
+     *              hostname  username  password  database
+     */
+
+    require_once '/etc/mysql-creds/ideashare-mysql.inc.php';
+    
+    // Create connection
+    $conn = mysqli_connect($ISM["host"], $ISM["user"], $ISM["pass"], $ISM["db"]);
+
+    // Check connection
+    if (mysqli_connect_errno()) {
+        die(mysqli_connect_error());
+    } else {
+        echo "MySQL Working";
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
-
-    /** How to access MySQL
-     *   1. Use the following require_once:
-     *      require_once '/etc/mysql-creds/ideashare-mysql.inc.php';
-     *   2. Use the array named $ISM
-     *   3. Fields: host,     user,     pass,     db
-     *              hostname  username  password  database
-     */
-    require_once '/etc/mysql-creds/ideashare-mysql.inc.php';
-    
-    echo $ISM["host"];
     echo "<br/>Welcome to the future site of IdeaShare!!";
 ?>
