52fe3932b4cb6b020e5651ce1bff8455bb52b3f5
Harry <zh.harry@yahoo.ca>
2016-09-17 02:45:48 +0000

    Testing Registration

--- auth/register.php
+++ auth/register.php
@@ -7,7 +7,7 @@
         print_r($_POST);
         
         $prep = mysqli_prepare($conn, "INSERT INTO is_users (username, email, frist, last, password) 
-            VALUES (?, ?, ?, ?, ?, ?);
+            VALUES (?, ?, ?, ?, ?);
         ");
         mysqli_stmt_bind_param($prep, 'sssss', $_POST["username"], $_POST["email"], $_POST["frist"], $_POST["last"], $_POST["password"]);
         mysqli_stmt_execute($prep);
