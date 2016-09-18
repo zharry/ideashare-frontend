ef321fa545eb57e2df41538c5735a5f2fa4af174
Harry <zh.harry@yahoo.ca>
2016-09-18 09:06:26 +0000

    Frontend Testing

--- includes/script.js
+++ includes/script.js
@@ -13,6 +13,8 @@ function createIdea() {
         }
     }
     api("ideas/create", [title, author, content, JSON.stringify(trimmedTags)], alert);
+    
+    window.location = "http://ideashare.ml";
 }
 function forceSearch() {
     var content = document.getElementById("topsearchquery").value;
