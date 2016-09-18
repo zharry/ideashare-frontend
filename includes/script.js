86603aac0915921131a3577981b0d5bb5e419bc5
Harry <zh.harry@yahoo.ca>
2016-09-18 06:43:16 +0000

    Frontend Testing

--- includes/script.js
+++ includes/script.js
@@ -30,7 +30,7 @@ function searchOut(response) {
     var tags = JSON.parse(response)["tags"];
     output += "<h6>Using tag cloud search with: ";
     for (var i = 0; i < tags.length; i++) {
-        output += "<span class=\"label label-primary\">" + tags[i] + "</span>";
+        output += "<span class=\"label label-primary\">" + tags[i] + "</span>&nbsp";
     }
     output += "</h6>";
     output += "<div class=\"list-group\">";
